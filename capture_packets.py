401cc5df6422e3009ea988c3a875108dfeee5cb6
gabrielclcardoso <gabrielclcardoso@gmail.com>
2025-07-18 17:16:33 -0300

    final version of launch demo

--- capture_packets.py
+++ capture_packets.py
@@ -4,9 +4,9 @@ import time
 
 # Configurações do InfluxDB
 influx_url = "http://localhost:8086"  # URL do seu InfluxDB
-influx_token = "EFuJl_vxykaQflccR6V8c4I0mMXce1z_QiYkHmBvKd7yJsQJqgGfRbD1qWav6fTbZWxWb30ZVpj0RFpNb-W24Q=="  # O token de autenticação
-org = "tupinymquim"  # Nome da sua organização
-bucket = "my-bucket"  # O nome do seu bucket
+influx_token = "ehyisCSiwzbeWJqbd3w6UNI4TiJpRS6ohQir4ZcqPlXUG2tzTkbKkhnmaOthAod00AeY_tMpTI6kLHPyAKJfbQ=="  # O token de autenticação
+org = "TupiNymQuim"  # Nome da sua organização
+bucket = "PacketCount"  # O nome do seu bucket
 
 # Criação do cliente InfluxDB
 client = InfluxDBClient(url=influx_url, token=influx_token)
@@ -14,33 +14,28 @@ client = InfluxDBClient(url=influx_url, token=influx_token)
 write_api = client.write_api()
 
 # Função para capturar pacotes
-def capture_packets(interface="wlp0s20f3"):
+def capture_packets(interface="wlp0s20f3", interval=5):
     # Inicia captura de pacotes na interface de rede
     cap = pyshark.LiveCapture(interface=interface)
 
+    start_time = time.time()
+    packet_count = 0
+
     for packet in cap:
         # Cria um ponto de dados para cada pacote
-        point = Point("network_packet")  # Nome da medida
+        packet_count += 1
+
+        if time.time() - start_time >= interval:
+            point = Point("network_packet").tag("type", "total").field("count", packet_count)
+            write_api.write(bucket=bucket, org=org, record=point)
+            points = []
+            start_time = time.time()
+            print(f"Pacotes enviados: {packet_count}")
+            packet_count = 0
 
         # Registra o contador total de pacotes
-        point.tag("type", "total")
-        point.field("count", 1)  # Cada pacote é contado com valor 1
-        write_api.write(bucket=bucket, org=org, record=point)
+        # write_api.write(bucket=bucket, org=org, record=point)
 
-        # Verifica o tipo de pacote e envia para o InfluxDB
-        if 'TCP' in packet:
-            point = Point("network_packet").tag("type", "tcp").field("count", 1)
-            write_api.write(bucket=bucket, org=org, record=point)
-        elif 'UDP' in packet:
-            point = Point("network_packet").tag("type", "udp").field("count", 1)
-            write_api.write(bucket=bucket, org=org, record=point)
-        elif 'ICMP' in packet:
-            point = Point("network_packet").tag("type", "icmp").field("count", 1)
-            write_api.write(bucket=bucket, org=org, record=point)
-
-        # Registra o tamanho do pacote
-        point = Point("network_packet_size").tag("type", "size").field("size_bytes", len(packet))
-        write_api.write(bucket=bucket, org=org, record=point)
 
 if __name__ == "__main__":
     print("Iniciando captura de pacotes...")
